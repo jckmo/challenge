4fc64829f48f203fda52d8a2df52ac5d769d5563
Jack <jacksonmorgan@hey.com>
2021-04-28 18:47:37 -0400

    remove unneeded functions in Search Results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 
 class SearchResults extends React.Component {
-
-  renderTitles = () => {
-    if (this.props.titles !== "") {
-      this.props.titles.map(title => {
-        return(
-          <div>
-            <p>{title.Title}</p>
-            <p>{title.Year}</p>
-          </div>
-        )
-      })
-    }
-  }
-
-  componentDidUpdate = () => {
-    this.render()
-  }
-
   render() {
-
     return (
       <>
       {this.props.titles === "" ? null : this.props.titles.map(title => <div><p>{title.Title}</p><p>{title.Year}</p></div>)}
