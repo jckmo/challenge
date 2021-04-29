36bfa2ef30c5e0b51f494b57c312f55dc9822c15
Jack <jacksonmorgan@hey.com>
2021-04-28 21:21:39 -0400

    fix uuid issue

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import {uuid} from 'react-uuid'
+import uuid from 'react-uuid'
 
 class SearchResults extends React.Component {
   render() {
     return (
       <>
-      {this.props.titles === "" ? null : this.props.titles.map(title => <div><p key={uuid()}>{title.Title}</p><p key={uuid()}>{title.Year}</p></div>)}
+      {this.props.titles === "" ? null : this.props.titles.map(title => {
+        return (
+          <div>
+            <p key={uuid()}>
+              {title.Title}
+            </p>
+            <p key={uuid()}>
+              {title.Year}
+            </p>
+          </div>
+        )}  
+      )}
       </>
     )
   }
